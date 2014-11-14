c3be67a694acb31980dd03f08ff46e8f77aa1845
Ricardo Almeida <ric.almeida@gmail.com>
2014-11-14 23:00:06 +0000

    No need to convert extra, it comes as json now

--- Skylanders/src/main/webapp/js/skyColApi.js
+++ Skylanders/src/main/webapp/js/skyColApi.js
@@ -15,15 +15,6 @@ var skyCol = {};
     
     loader.wait(function(){
         var origItemList = colApi.item.list;
-        colApi.item.list = function() {
-            return origItemList.apply(this, arguments).done(function(items) {
-                items.forEach(function(item) {
-                    if (item.extra) {
-                        item.extra = JSON.parse(item.extra);
-                    }
-                });
-            });
-        };
         
         colApi.user.currentUser().done(function(user) {
             $(document).ready(function() {
