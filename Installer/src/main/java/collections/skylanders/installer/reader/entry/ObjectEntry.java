d71f1fe76bf3290703dcec52b2f205f274e102bc
Ricardo Almeida <ric.almeida@gmail.com>
2015-02-18 21:29:44 +0000

    Replace tabs with 4 spaces

--- Installer/src/main/java/collections/skylanders/installer/reader/entry/ObjectEntry.java
+++ Installer/src/main/java/collections/skylanders/installer/reader/entry/ObjectEntry.java
@@ -1,13 +1,13 @@
 package collections.skylanders.installer.reader.entry;
 
 public abstract class ObjectEntry {
-	protected final String name;
-	
-	protected ObjectEntry(String name) {
-		this.name = name;
-	}
-	
-	public String getName() {
-		return name;
-	}
+    protected final String name;
+    
+    protected ObjectEntry(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return name;
+    }
 }
